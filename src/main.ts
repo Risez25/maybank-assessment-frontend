43036ae2bab1aecc9d8f962cf8e641f33704666a
hafiz <muhamadhafiz93@gmail.com>
2020-04-30 01:54:02 +0800

    add production environment

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
-﻿import './polyfills';
+﻿import "./polyfills";
 
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { AppModule } from "./app/app.module";
+import { environment } from "./environment/environment.prod";
+import { enableProdMode } from "@angular/core";
+if (environment.production) {
+  enableProdMode();
+}
 platformBrowserDynamic().bootstrapModule(AppModule);
